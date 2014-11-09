1900acba877b9358030478d398677a33d4f50608
Archangel_SDY <Archangel.SDY@gmail.com>
2014-11-09 10:29:15 +0800

    Add CMake support.

--- Qt7z/Qt7zPackage.cpp
+++ Qt7z/Qt7zPackage.cpp
@@ -1,8 +1,8 @@
-#include "7z.h"
-#include "7zAlloc.h"
-#include "7zCrc.h"
-#include "7zFile.h"
-#include "7zVersion.h"
+#include "7z/7z.h"
+#include "7z/7zAlloc.h"
+#include "7z/7zCrc.h"
+#include "7z/7zFile.h"
+#include "7z/7zVersion.h"
 
 #include "Qt7zPackage.h"
 
